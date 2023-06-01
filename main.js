d66fb8c68188ca04a077a60a06c398647307377c
Gabriell Henrique <42591225+GabriellPrado@users.noreply.github.com>
2023-06-01 09:40:36 -0300

    Commit inicial

---
+++ main.js
@@ -0,0 +1,15 @@
+function calcularPadding() {
+    const larguraInput = document.getElementById('larguraInput');
+    const alturaInput = document.getElementById('alturaInput');
+    const resultadoPadding = document.getElementById('resultadoPadding');
+  
+    const largura = parseFloat(larguraInput.value);
+    const altura = parseFloat(alturaInput.value);
+  
+    if (isNaN(largura) || isNaN(altura)) {
+      resultadoPadding.textContent = 'Insira valores válidos para largura e altura.';
+    } else {
+      const proporcao = (altura / largura) * 100;
+      resultadoPadding.textContent = `Padding-bottom: ${proporcao}%`;
+    }
+}
