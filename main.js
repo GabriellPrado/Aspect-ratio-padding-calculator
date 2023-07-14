cdd68e2feee536385455e31051967bb2eae9fabe
Gabriell Henrique <42591225+GabriellPrado@users.noreply.github.com>
2023-07-14 14:25:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 function calcularPadding() {
-    const larguraInput = document.getElementById('larguraInput');
-    const alturaInput = document.getElementById('alturaInput');
-    const resultadoPadding = document.getElementById('resultadoPadding');
-  
-    const largura = parseFloat(larguraInput.value);
-    const altura = parseFloat(alturaInput.value);
-  
-    if (isNaN(largura) || isNaN(altura)) {
-      resultadoPadding.textContent = 'Insira valores válidos para largura e altura.';
-    } else {
-      const proporcao = (altura / largura) * 100;
-      resultadoPadding.textContent = `position: relative;<br>padding-bottom: ${proporcao}%;`;
-    }
+  const larguraInput = document.getElementById('larguraInput');
+  const alturaInput = document.getElementById('alturaInput');
+  const resultadoPadding = document.getElementById('resultadoPadding');
+
+  const largura = parseFloat(larguraInput.value);
+  const altura = parseFloat(alturaInput.value);
+
+  if (isNaN(largura) || isNaN(altura)) {
+    resultadoPadding.innerHTML = 'Insira valores válidos para largura e altura.';
+  } else {
+    const proporcao = (altura / largura) * 100;
+    resultadoPadding.innerHTML = `position: relative;<br>width: 100%;<br>padding-bottom: ${proporcao}%;`;
+  }
 }
